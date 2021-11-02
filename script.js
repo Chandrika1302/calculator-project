03e340e142a168d35576039a816ce2310f2e8e17
Chandrika <kummarakuntla.11901601@lpu.in>
2021-11-02 16:52:46 +0530

    add code to answer screen

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-function add(a,b){
-    return a+b;
-}
-function sub(a,b){
-    return a-b;
-}
-function mul(a,b){
-    return a*b;
-}
-function div(a,b){
-    return a/b;
-}
+// function add(a, b) {
+//   return a + b;
+// }
+// function sub(a, b) {
+//   return a - b;
+// }
+// function mul(a, b) {
+//   return a * b;
+// }
+// function div(a, b) {
+//   return a / b;
+// }
 
-function operate(num1,num2,operator){
-    switch(operator)
-    {
-        case '+':
-            return add(num1,num2);
-        case '-':
-            return sub(num1,num2);
-        case '*':
-            return mul(num1,num2);
-        case '/':
-            return div(num1,num2);
-        default:
-            console.log('choose correct option');
-    }
-}
+// function operate(num1, num2, operator) {
+//   switch (operator) {
+//     case "+":
+//       return add(num1, num2);
+//     case "-":
+//       return sub(num1, num2);
+//     case "*":
+//       return mul(num1, num2);
+//     case "/":
+//       return div(num1, num2);
+//     default:
+//       console.log("choose correct option");
+//   }
+// }
 
-document.getElementById('add-btn').onclick=function(){
-console.log(operate(4,2,'*'));
-console.log(operate(4,2,'+'));
-console.log(operate(4,2,'-'));
-console.log(operate(4,2,'/'));
+// document.getElementById("add-btn").onclick = function () {
+//   console.log(operate(4, 2, "*"));
+//   console.log(operate(4, 2, "+"));
+//   console.log(operate(4, 2, "-"));
+//   console.log(operate(4, 2, "/"));
+// };
 
+const numOfCalcibtn=document.querySelectorAll('.calci-btn').length;
+const screenView=document.querySelector(".ans-screen");
+
+for(let i=0;i<=numOfCalcibtn;i++){
+  document.querySelectorAll(".calci-btn")[i].addEventListener('click',function(){
+    let calcibtnInnerHtml=this.innerText;
+    screenView.innerText+=calcibtnInnerHtml;
+  })
 }
