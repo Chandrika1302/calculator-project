e23933b0e85dd5ac3bf3dcb802443c6947aa24cb
Chandrika <kummarakuntla.11901601@lpu.in>
2021-11-07 16:35:20 +0530

    add foreach loop

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const numButtons=document.querySelectorAll("[data-number]");
-const operationBtns=document.querySelectorAll("[data-opertion]");
-const equalBtn=document.querySelector("[data-equals]");
-const deleteBtn=document.querySelector("[data-delete]");
-const allClearBtn=document.querySelector("[data-clear]");
-const oldText=document.querySelector("[data-previous]");
-const currentText=document.querySelector("[data-current]");
+const numButtons=[...document.querySelectorAll("[data-number]")];
+const operationBtns=[...document.querySelectorAll("[data-opertion]")];
+const equalBtn=[...document.querySelector("[data-equals]")];
+const deleteBtn=[...document.querySelector("[data-delete]")];
+const allClearBtn=[...document.querySelector("[data-clear]")];
+const oldText=[...document.querySelector("[data-previous]")];
+const currentText=[...document.querySelector("[data-current]")];
 
-
-  
-
+// console.log(numButtons,operationBtns,equalBtn,deleteBtn,allClearBtn,oldText,currentText);
+numButtons.forEach(button =>{
+  button.addEventListener("click",()=> {
+      console.log("working!");
+  });
+});
