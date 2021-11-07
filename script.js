84be10c2fe2e0e9f9e235dcd34cfe76898be18c1
Chandrika <kummarakuntla.11901601@lpu.in>
2021-11-07 18:21:53 +0530

    implement basic display function

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const numButtons=[...document.querySelectorAll("[data-number]")];
-const operationBtns=[...document.querySelectorAll("[data-opertion]")];
-const equalBtn=[...document.querySelector("[data-equals]")];
-const deleteBtn=[...document.querySelector("[data-delete]")];
-const allClearBtn=[...document.querySelector("[data-clear]")];
-const oldText=[...document.querySelector("[data-previous]")];
-const currentText=[...document.querySelector("[data-current]")];
 
-// console.log(numButtons,operationBtns,equalBtn,deleteBtn,allClearBtn,oldText,currentText);
+const numButtons=[...document.querySelectorAll("[data-number]")];
+const operationBtns=[...document.querySelectorAll("[data-operation]")];
+const equalBtn=document.querySelector("[data-equals]");
+const deleteBtn=document.querySelector("[data-delete]");
+const allClearBtn=document.querySelector("[data-clear]");
+const oldText=document.querySelector("[data-previous]");
+const currentText=document.querySelector("[data-current]");
+
+let oldValue="";
+let currentValue="";
+let operator=null;
+
 numButtons.forEach(button =>{
-  button.addEventListener("click",()=> {
-      console.log("working!");
+  button.addEventListener("click",(e)=> {
+  appendNumber(e.target.innerText)
+    display();
   });
 });
+
+operationBtns.forEach(button => {
+  button.addEventListener("click",()=>{
+  });
+});
+
+function display(){
+  currentText.textContent=currentValue;
+}
+function appendNumber(newNumber){
+  if(newNumber==='.' && currentValue.includes('.')) return;
+  currentValue+=newNumber;
+}
