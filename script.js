7f1cd0d561ed0b9302363aa8a758a9a53c77185d
Chandrika <kummarakuntla.11901601@lpu.in>
2021-11-07 19:14:24 +0530

    implement basic clear and choose operation  functions

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
+const numButtons = [...document.querySelectorAll("[data-number]")];
+const operationBtns = [...document.querySelectorAll("[data-operation]")];
+const equalBtn = document.querySelector("[data-equals]");
+const deleteBtn = document.querySelector("[data-delete]");
+const allClearBtn = document.querySelector("[data-clear]");
+const oldText = document.querySelector("[data-previous]");
+const currentText = document.querySelector("[data-current]");
 
-const numButtons=[...document.querySelectorAll("[data-number]")];
-const operationBtns=[...document.querySelectorAll("[data-operation]")];
-const equalBtn=document.querySelector("[data-equals]");
-const deleteBtn=document.querySelector("[data-delete]");
-const allClearBtn=document.querySelector("[data-clear]");
-const oldText=document.querySelector("[data-previous]");
-const currentText=document.querySelector("[data-current]");
+let oldValue = "";
+let currentValue = "";
+let operator = "";
 
-let oldValue="";
-let currentValue="";
-let operator=null;
-
-numButtons.forEach(button =>{
-  button.addEventListener("click",(e)=> {
-  appendNumber(e.target.innerText)
+numButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    appendNumber(e.target.innerText);
     display();
   });
 });
 
-operationBtns.forEach(button => {
-  button.addEventListener("click",()=>{
+operationBtns.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    chooseOperation(e.target.innerText);
+    display();
   });
 });
 
-function display(){
-  currentText.textContent=currentValue;
+allClearBtn.addEventListener("click", () => {
+  clear();
+  display();
+});
+
+function display() {
+  currentText.textContent = currentValue;
+  oldText.textContent = `${oldValue} ${operator}`  
 }
-function appendNumber(newNumber){
-  if(newNumber==='.' && currentValue.includes('.')) return;
-  currentValue+=newNumber;
+function appendNumber(newNumber) {
+  if (newNumber === "." && currentValue.includes(".")) return;
+  currentValue += newNumber;
+}
+function clear() {
+  currentValue = "";
+  oldValue = "";
+  operator = "";
+}
+function chooseOperation(newOperator) {
+  if(currentValue==="") return;
+  operator = newOperator;
+  oldValue=currentValue;
+  currentValue="";
 }
